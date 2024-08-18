ecd5adbea22670301c1f120f9a42f97ca42d9576
Tzu-Chao Wang <john29917958@gmail.com>
2024-08-18 23:06:26 +0800

    Add calculator program

---
+++ include/calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int sum(int addend1, int addend2);
+
+int subtract(int minuend, int subtrahend);
+
+int multiply(int multiplier, int multiplicand);
+
+int divide(int dividend, int divisor);
+
+#endif
---
+++ src/calculator.c
@@ -0,0 +1,25 @@
+#include "calculator.h"
+
+int sum(int addend1, int addend2)
+{
+    int sum = addend1 + addend2;
+    return sum;
+}
+
+int subtract(int minuend, int subtrahend)
+{
+    int difference = minuend - subtrahend;
+    return difference;
+}
+
+int multiply(int multiplier, int multiplicand)
+{
+    int product = multiplier * multiplicand;
+    return product;
+}
+
+int divide(int dividend, int divisor)
+{
+    int quotient = dividend / divisor;
+    return quotient;
+}
